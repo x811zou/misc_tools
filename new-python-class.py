63900ed3f2a69db35a79251002e764e270399311
Bill Majoros <martiandna@gmail.com>
2016-10-08 14:19:46 -0400

    new-python-class.py

---
+++ new-python-class.py
@@ -0,0 +1,51 @@
+#!/bin/env python
+#=========================================================================
+# This is OPEN SOURCE SOFTWARE governed by the Gnu General Public
+# License (GPL) version 3, as described at www.opensource.org.
+# Copyright (C)2016 William H. Majoros (martiandna@gmail.com).
+#=========================================================================
+from __future__ import (absolute_import, division, print_function, 
+   unicode_literals, generators, nested_scopes, with_statement)
+from builtins import (bytes, dict, int, list, object, range, str, ascii,
+   chr, hex, input, next, oct, open, pow, round, super, filter, map, zip)
+import os
+import sys
+import ProgramName
+
+# Process command line
+name=ProgramName.get();
+if(len(sys.argv)!=2):
+    sys.exit(name+" <classname>")
+className=sys.argv[1]
+
+# Write file
+filename=className+".py"
+if(os.path.exists(filename)):
+    sys.exit(filename+" exists");
+fh=open(filename,"w")
+header="\n".join(["#=========================================================================",
+    "# This is OPEN SOURCE SOFTWARE governed by the Gnu General Public",
+    "# License (GPL) version 3, as described at www.opensource.org.",
+    "# Copyright (C)2016 William H. Majoros (martiandna@gmail.com).",
+    "#=========================================================================",
+    "from __future__ import (absolute_import, division, print_function",
+    "   unicode_literals, generators, nested_scopes, with_statement)",
+    "from builtins import (bytes, dict, int, list, object, range, str, ascii,",
+    "   chr, hex, input, next, oct, open, pow, round, super, filter, map, zip)",
+    "\n"
+    "#=========================================================================",
+    "# Attributes:",
+    "#   ",
+    "# Methods:",
+    "#   "+className+"()",
+    "#=========================================================================\n",
+    ])
+fh.write(header)
+fh.write("class "+className+":\n")
+fh.write("    \"\"\""+className+"\"\"\"\n");
+fh.write("    def __init__(self):\n")
+fh.write("        pass\n")
+fh.write("\n\n\n")
+fh.close()
+
+
